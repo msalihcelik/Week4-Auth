3ca30900a0041da15dc77b5e0f9d1b41269fdafc
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2021-10-11 15:19:45 +0300

    Update

--- Week4-Auth/UserInformationViewController.swift
+++ Week4-Auth/UserInformationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignUpViewController.swift
+//  Step2ViewController.swift
 //  Week4-Auth
 //
 //  Created by Mehmet Salih ÇELİK on 5.10.2021.
@@ -7,21 +7,35 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController, UITextFieldDelegate {
+class UserInformationViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var emailConfirmTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var retypePasswordTextField: UITextField!
-    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var street1TextField: UITextField!
+    @IBOutlet weak var street2TextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var postcodeTextField: UITextField!
     @IBOutlet weak var buttonTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var continueButton: UIButton!
     var buttonConstraint: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addTapGesture()
         configureContents()
+        setDelegate()
+        addTapGesture()
         observeKeyboard()
+    }
+    
+    func setDelegate() {
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        street1TextField.delegate = self
+        street2TextField.delegate = self
+        stateTextField.delegate = self
+        countryTextField.delegate = self
+        postcodeTextField.delegate = self
     }
     
     func addTapGesture() {
@@ -35,48 +49,8 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     }
     
     func configureContents() {
-        emailTextField.delegate = self
-        emailConfirmTextField.delegate = self
-        passwordTextField.delegate = self
-        retypePasswordTextField.delegate = self
         continueButton.layer.cornerRadius = 10
         buttonConstraint = buttonTopConstraint.constant
-    }
-    
-    func setNavigationBar() {
-        let navigationBar = navigationController?.navigationBar
-        navigationBar?.isTranslucent = false
-        navigationBar?.setBackgroundImage(UIImage(), for: .default)
-        navigationBar?.shadowImage = UIImage()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        setNavigationBar()
-    }
-    
-    @IBAction func continueButtonTapped(_ sender: Any) {
-        guard (emailTextField.text?.isEmpty == false) && (passwordTextField.text?.isEmpty == false) else {
-            let alert = UIAlertController(title: "Password or email cannot be empty",
-                                          message: "",
-                                          preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK",
-                                         style: .default)
-            alert.addAction(okAction)
-            self.present(alert, animated: true)
-            return
-        }
-        
-        if (emailTextField.text == emailConfirmTextField.text) && (passwordTextField.text == retypePasswordTextField.text) {
-            performSegue(withIdentifier: "toUserInformationVC", sender: nil)
-        } else {
-            let alert = UIAlertController(title: "Password or email do not match",
-                                          message: "",
-                                          preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK",
-                                         style: .default)
-            alert.addAction(okAction)
-            self.present(alert, animated: true)
-        }
     }
     
     func observeKeyboard() {
@@ -89,7 +63,7 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
                                                name: UIResponder.keyboardWillHideNotification,
                                                object: nil)
     }
-    
+
     @objc
     func keyboardWillShow(_ notification: Notification) {
         let keyboardHeight = getKeyboardHeight(notification: notification)
